787f6416555c138e927517fa84b721a7a4d7cb57
Darío Javier Cravero <dario@qinnova.com.ar>
2013-01-28 02:40:41 -0300

    Added some docs on the API when the web interface is browsed.

--- web/app.rb
+++ web/app.rb
@@ -5,20 +5,30 @@ require 'cdnio'
 module CDNio 
   class WebApp < Sinatra::Application
     set :cdn, CDN.new
+    set :layout, :layout
+    set :format, :html5
 
-    get '/', :provides => :json do
-      settings.cdn.libraries(params.include?(:provider) ? params[:provider] : :all).to_json
+    get '/', :provides => [:json, :html] do
+      if content_type =~ /json/
+        settings.cdn.libraries(params.include?(:provider) ? params[:provider] : :all).to_json
+      elsif content_type =~ /html/
+        haml :index
+      end
     end
 
-    get '/latest/:library', :provides => :json do
-      library = params[:library]
-      provider = params.include?('provider') ? params['provider'] : :all
+    get '/latest/:library', :provides => [:json, :html] do
+      if content_type =~ /json/
+        library = params[:library]
+        provider = params.include?('provider') ? params['provider'] : :all
 
-      latest = settings.cdn.latest(library, provider)
+        latest = settings.cdn.latest(library, provider)
 
-      halt 404, {:error => "Couldn't find #{library} #{provider == :all ? 'at all' : %{in #{provider}}}."}.to_json if latest.nil?
+        halt 404, {:error => "Couldn't find #{library} #{provider == :all ? 'at all' : %{in #{provider}}}."}.to_json if latest.nil?
 
-      latest.to_json
+        latest.to_json
+      elsif content_type =~ /html/
+        haml :latest
+      end
     end
 
     # Start the server if ruby file executed directly
